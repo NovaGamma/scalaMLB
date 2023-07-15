5ea341a0ae5dad34d71187e1c9bb0c805beae276
popolito1 <paulineblanc92@gmail.com>
2023-07-15 20:17:32 +0200

    fix: add comments to tests

--- src/test/scala/MlbApiSpec.scala
+++ src/test/scala/MlbApiSpec.scala
@@ -8,32 +8,47 @@ import zio.jdbc._
 
 class MlbApiSpec extends munit.ZSuite {
 
+  //Add the static routes and the endpoints from MlbApi
   val app: App[Any] = MlbApi.static
   val endpoints: Http[ZConnectionPool, Response, Request, Response] = MlbApi.endpoints
 
 
+/**
+  * Test the static /text endpoint of the app
+  */
   testZ("Static route /text : should be ok ") {
     val req = Request.get(URL(Root / "text"))
     assertZ(app.runZIO(req).isSuccess)
   }
 
+  /**
+   * // Test the static  /json endpoint of the app
+  */
   testZ("Static route Json : should be ok") {
     val req = Request.get(URL(Root / "json"))
     assertZ(app.runZIO(req).isSuccess)
   }
 
+  /**
+    *  Test an endpoint that should return Not Found
+    */
   testZ("should be Not Found") {
     val req = Request.get(URL(Root))
     assertZ(endpoints.runZIO(req).isFailure)
   }
 
+  /**
+   * Test the /init endpoint
+   */
   testZ("should be initialized") {
     val req = Request.get(URL(Root / "init"))
     assertZ(endpoints.runZIO(req).isSuccess)
     assert(endpoints.runZIO(req).map(_.status.code == Status.NotImplemented.code))
 
   }
-  
+  /**
+   * Test the /game/latest/{homeTeam}/{awayTeam} endpoint
+   */
   testZ("should be games latest") {
     val homeTeam = "ATL"
     val awayTeam = "NYM"
@@ -45,6 +60,9 @@ class MlbApiSpec extends munit.ZSuite {
     assertZ(result.map(_.status.code == Status.Ok.code))
   }
 
+  /**
+    * Test the /game/predict/{homeTeam}/{awayTeam} endpoint
+    */
   testZ("should be games predict") {
     val homeTeam = "ATL"
     val awayTeam = "NYM"
@@ -56,6 +74,9 @@ class MlbApiSpec extends munit.ZSuite {
     assertZ(result.map(_.status.code == Status.Ok.code))
   } 
 
+  /**
+    * Test the /games/count endpoint
+    */
   testZ("should be games count") {
     val req = Request.get(URL(Root / "games" / "count"))
     val result = endpoints.runZIO(req)
@@ -64,12 +85,18 @@ class MlbApiSpec extends munit.ZSuite {
     assertZ(result.map(_.status.code == Status.Ok.code))
   }
 
+  /**
+    * Test an endpoint that returns no game in the historical data
+    */
   testZ("should be No game in historical data") {
     val req = Request.get(URL(Root / "games" / "count_fail"))
     assertZ(endpoints.runZIO(req).isSuccess)
     assertZ(endpoints.runZIO(req).map(_.status).map(_.code).map(_ == 204))
   }
 
+  /**
+    * Test the /games/history/{homeTeam} endpoint
+    */
   testZ("should be game history") {
     val homeTeam = "ATL"
     val req = Request.get(URL(Root / "games" / "history" / homeTeam))
@@ -78,7 +105,10 @@ class MlbApiSpec extends munit.ZSuite {
     assertZ(result.isSuccess)
     assertZ(result.map(_.status.code == Status.Ok.code))
   } 
-
+  
+  /**
+  * Test the /pitcher/history/{pitcher} endpoint
+  */
   testZ("should be pitcher history") {
     val pitcher = "German Marquez"
     val req = Request.get(URL(Root / "pitcher" / "history" / pitcher))
